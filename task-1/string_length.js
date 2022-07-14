365cbb4f091bbcfb47161af1beda652a44ee1c2d
Fourteen98 <emuhiydeen6@gmail.com>
2022-07-14 18:49:20 +0000

    add task 1 and 2

---
+++ task-1/string_length.js
@@ -0,0 +1,7 @@
+export default function stringLength(string) {
+  if (string.length < 1 || string.length > 10) {
+    throw new Error('String must be between 1 and 10 characters');
+  }
+  return string.length;
+}
+
---
+++ task_2/string_reverse.test.js
@@ -0,0 +1,8 @@
+import reverseString from "./reverse_string.js";
+// write at least one test function
+describe('reverseString', () => {
+  it('should return the string reversed', () => {
+    expect(reverseString('string')).toBe('gnirts');
+  }
+  );
+});
